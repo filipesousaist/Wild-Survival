b425018532f5bb5e1c954130939f5e023f21511d
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2020-11-26 12:00:33 +0000

    normalizing and LadyZombie movement animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -44,7 +44,7 @@ public class PlayerMovement : MonoBehaviour
     void MoveCharacter()
     {
         myRigidBody.MovePosition(
-            transform.position + change * speed * Time.deltaTime
+            transform.position + change.normalized * speed * Time.deltaTime
         );
     }
 }
