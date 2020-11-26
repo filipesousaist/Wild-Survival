c38592b10d1d58c9eb6d3cdf1132188fbf9efd64
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2020-11-26 18:00:19 +0000

    Zombie following Player

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public GameObject player;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = player.transform.position.x - transform.position.x;
+        change.y = player.transform.position.y - transform.position.y;
+        if (change.magnitude < 0.1)
+            change = Vector3.zero;
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidBody.MovePosition(
+            transform.position + change.normalized * speed * Time.deltaTime
+        );
+    }
+}
