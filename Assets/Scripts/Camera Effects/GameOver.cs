b780f1c0576cd7be71bbbfae61e760248313a474
j-galveias <brainsuprajr@gmail.com>
2020-12-27 20:14:09 +0000

    possible death screen crash fixes

--- Assets/Scripts/Camera Effects/GameOver.cs
+++ Assets/Scripts/Camera Effects/GameOver.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    public GameObject activists;
     public void Quit()
     {
         //nao funciona enquanto Testamos no unity
@@ -11,6 +12,8 @@ public class GameOver : MonoBehaviour
 
     public void Reset()
     {
+        ActivistsManager activistsManager = activists.GetComponent<ActivistsManager>();
+        activistsManager.currentPlayer.value = 0;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
     }
