7b02ed5bcf8de9f9ab8d2f3f459baea2fba14d2d
André Barrocas <andrebarrocas99@hotmail.com>
2021-01-09 23:09:12 +0000

    FoodManager

--- Assets/Scripts/Managers/FoodManager.cs
+++ Assets/Scripts/Managers/FoodManager.cs
@@ -1,18 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FoodManager : MonoBehaviour
+﻿using UnityEngine;
+using UnityEngine.UI;
+public abstract class CharacterInfoManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image portrait;
+
+    // Name/level
+    public GameObject nameLevelBar;
+    private Text nameText;
+    private Text levelText;
+
+    // Food
+    public GameObject foodBar;
+    private Transform foodMidRect;
+    private Transform foodFrontRect;
+    private Text foodText;
+
+    public FloatValue currentFood;
+
+    protected ActivistsManager activistsManager;
+
+    protected virtual void Awake()
     {
-        
+        InitFood();
+
+        nameText = nameLevelBar.transform.Find("NameBar").Find("Text").GetComponent<Text>();
+        levelText = nameLevelBar.transform.Find("LevelBar").Find("Text").GetComponent<Text>();
+
+        Transform foodTransform = foodBar.transform.Find("Bar");
+        foodMidRect = foodTransform.Find("Middle Rect");
+        foodFrontRect = foodTransform.Find("Front Rect");
+        foodText = foodBar.transform.Find("Food Amount Text").GetComponent<Text>();
+
+        activistsManager = FindObjectOfType<ActivistsManager>();
+    }
+    
+    public void InitFood()
+    {
+        foodBar.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void UpdateFoodBar()
     {
-        
+        Character character = GetCurrentCharacter();
+
+        float food = Mathf.Max(currentFood.value, 0);
+        float percentage = food / character.maxFood;
+        float newPosition = - foodMidRect.localScale.x / 2;
+        float newWidth = foodMidRect.localScale.x * percentage;
+        foodFrontRect.localScale = new Vector3(newWidth, foodMidRect.localScale.y);
+        foodFrontRect.localPosition = new Vector3(newPosition, foodMidRect.localPosition.y);
+
+        foodFrontRect.GetComponent<Image>().color = character.ChooseBarColor(percentage);
+
+        foodText.text = food + "/" + character.maxFood;
     }
+
+
+    public void UpdatePortrait()
+    {
+        // Can remove when all activists have a rhino
+        FindObjectOfType<RhinosManager>().ToggleRhinoInfo();
+
+        Character character = GetCurrentCharacter();
+        if (character != null)
+        {
+            portrait.sprite = character.portrait;
+            nameText.text = character.entityName;
+        }
+    }
+
+    protected abstract Character GetCurrentCharacter();
 }
